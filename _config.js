86cff6fbe4320e8df155a3b598fec6741600bd74
geekydee <doreen.mburungah@gmail.com>
2024-07-04 22:04:27 +0300

    added db connecting string

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+	production: 'mongodb+srv://dorenembrngh88:1Mff1PWCpVX8zh0H@mscluster.dyu9se3.mongodb.net/?retryWrites=true&w=majority&appName=MSCluster'
+    development: 'mongodb+srv://dorenembrngh88:1Mff1PWCpVX8zh0H@mscluster.dyu9se3.mongodb.net/?retryWrites=true&w=majority&appName=MSCluster'
+    test: 'mongodb+srv://dorenembrngh88:1Mff1PWCpVX8zh0H@mscluster.dyu9se3.mongodb.net/?retryWrites=true&w=majority&appName=MSCluster'
 }
 module.exports = config;
