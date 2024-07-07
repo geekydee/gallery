97373b4f311b25dc69d2e93e8556884546038be9
Kajuju <dee.tools21@gmail.com>
2024-07-08 00:16:40 +0300

    Update _config.js

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-	production: 'mongodb+srv://kajuju:6jX9YhWWrL7gvdd4@gallery.clusterms-wk2-project.zlftvug.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Clusterms-wk2-project',
-	development: 'mongodb+srv://kajuju:6jX9YhWWrL7gvdd4@gallery.clusterms-wk2-project.zlftvug.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=Clusterms-wk2-project',
-	test: 'mongodb+srv://kajuju:6jX9YhWWrL7gvdd4@gallery.clusterms-wk2-project.zlftvug.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=Clusterms-wk2-project',
+	production: 'mongodb+srv://kajuju:6jX9YhWWrL7gvdd4@clusterms-wk2-project.zlftvug.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Clusterms-wk2-project',
+	development: 'mongodb+srv://kajuju:6jX9YhWWrL7gvdd4@clusterms-wk2-project.zlftvug.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=Clusterms-wk2-project',
+	test: 'mongodb+srv://kajuju:6jX9YhWWrL7gvdd4@clusterms-wk2-project.zlftvug.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=Clusterms-wk2-project',
 }
 module.exports = config;
